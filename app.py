ad279be05d270cab52fd65ecb2123a35cd21898c
Darya Buturlakina <darja020302@gmail.com>
2025-05-23 15:55:06 +0500

    Security update

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+# app.py
 from flask import Flask
 
 app = Flask(__name__)
+
+# Добавляем security headers
+@app.after_request
+def add_security_headers(response):
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'DENY'
+    return response
 
 @app.route('/')
 def home():
